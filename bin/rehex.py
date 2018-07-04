fcd46b4251c52d57167a5295b60d1c727b790f8d
phaseproject <40709480+phaseproject@users.noreply.github.com>
2018-07-05 00:30:08 +0300

    Update rehex.py

--- bin/rehex.py
+++ bin/rehex.py
@@ -6,7 +6,7 @@ from pprint import pprint
 import sys
 import os
 sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), '../lib')))
-import protonlib
+import phaselib
 # ============================================================================
 usage = "%s <hex>" % sys.argv[0]
 
@@ -15,7 +15,7 @@ if len(sys.argv) < 2:
     print(usage)
     sys.exit(1)
 else:
-    obj = protonlib.deserialise(sys.argv[1])
+    obj = phaselib.deserialise(sys.argv[1])
 
 pdb.set_trace()
 1
